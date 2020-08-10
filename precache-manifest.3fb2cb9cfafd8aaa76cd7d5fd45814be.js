00f779bc7effc548222dd2f89fe08d1512aadb09
Sergei <sklokovth@gmail.com>
2020-08-10 06:28:57 +0300

    Updates

---
+++ precache-manifest.3fb2cb9cfafd8aaa76cd7d5fd45814be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eeb4dc7fee4e5991215f7cf68311b03",
+    "url": "/LibrariesStatistics/index.html"
+  },
+  {
+    "revision": "2099f113c5050fbe6b97",
+    "url": "/LibrariesStatistics/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "26e5c94bb23538724c4f",
+    "url": "/LibrariesStatistics/static/css/main.5e363b45.chunk.css"
+  },
+  {
+    "revision": "2099f113c5050fbe6b97",
+    "url": "/LibrariesStatistics/static/js/2.23724325.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/LibrariesStatistics/static/js/2.23724325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e5c94bb23538724c4f",
+    "url": "/LibrariesStatistics/static/js/main.d7dc089a.chunk.js"
+  },
+  {
+    "revision": "7f3803fe453bb265e204",
+    "url": "/LibrariesStatistics/static/js/runtime-main.f72a8be5.js"
+  }
+]);
